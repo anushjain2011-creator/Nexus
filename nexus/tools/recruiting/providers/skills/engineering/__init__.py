e880f7d13ef3add2df6207557d09f1ed2550dd1d
anushjain2011-creator <anushjain2011@gmail.com>
2026-07-22 16:13:12 -0400

    Add __init__.py for engineering skills module

---
+++ nexus/tools/recruiting/providers/skills/engineering/__init__.py
@@ -0,0 +1,27 @@
+from .coding import CodingSkill
+from .architecture import ArchitectureSkill
+from .debugging import DebuggingSkill
+from .testing import TestingSkill
+from .devops import DevOpsSkill
+from .security import SecuritySkill
+from .databases import DatabaseSkill
+from .api_design import APIDesignSkill
+from .documentation import DocumentationSkill
+from .code_review import CodeReviewSkill
+from .deployment import DeploymentSkill
+from .refactoring import RefactoringSkill
+
+__all__ = [
+    "CodingSkill",
+    "ArchitectureSkill",
+    "DebuggingSkill",
+    "TestingSkill",
+    "DevOpsSkill",
+    "SecuritySkill",
+    "DatabaseSkill",
+    "APIDesignSkill",
+    "DocumentationSkill",
+    "CodeReviewSkill",
+    "DeploymentSkill",
+    "RefactoringSkill",
+]
